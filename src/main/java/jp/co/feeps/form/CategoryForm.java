83147fb238d191398d5b2024afbe3c729b6bb4a6
taka-yu-ki <taka7_zegs@icloud.com>
2025-01-02 13:44:36 +0900

    カテゴリ登録機能_API

---
+++ src/main/java/jp/co/feeps/form/CategoryForm.java
@@ -0,0 +1,12 @@
+package jp.co.feeps.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryForm {
+	private String name;
+	private String color;
+	private int teamId;
+}
